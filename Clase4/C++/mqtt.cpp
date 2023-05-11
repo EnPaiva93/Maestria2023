49a08bdb9360cb88594954f1d42ca8b496e67a15
Enrique Paiva <enpaiva93@gmail.com>
2023-05-11 07:56:27 -0400

    Update mqtt.cpp

--- Clase4/C++/mqtt.cpp
+++ Clase4/C++/mqtt.cpp
@@ -46,23 +46,26 @@ void off(const std::string& ip, const std::string& topic){
 
 int main()
 {
-    std::string ip = "192.168.0.17";
+    std::string ip = "localhost";
     for (int i = 0; i < 100; i++) {
-        enviar_mensaje(ip, "alu1", i);
-        enviar_mensaje(ip, "alu2", i);
-        enviar_mensaje(ip, "alu3", i);
+        enviar_mensaje(ip, "alu4", i);
+        enviar_mensaje(ip, "alu5", i);
+        enviar_mensaje(ip, "alu6", i);
         // Wait for 1 second before sending the next message
         std::this_thread::sleep_for(std::chrono::seconds(1));
         if(i%2 == 0){
-            on(ip, "alu4");
-            off(ip, "alu5");
-            on(ip, "alu6");
+            on(ip, "alu1");
+            off(ip, "alu2");
+            on(ip, "alu3");
         } else {
-            off(ip, "alu4");
-            on(ip, "alu5");
-            off(ip, "alu6");
+            off(ip, "alu1");
+            on(ip, "alu2");
+            off(ip, "alu3");
         }
+
+        enviar_mensaje(ip, "alu7", i);
+        enviar_mensaje(ip, "alu8", i);
+        enviar_mensaje(ip, "alu9", i);
     }
     return 0;
 }
-
